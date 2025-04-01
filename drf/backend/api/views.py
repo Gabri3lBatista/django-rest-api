05215919a0fdd19c95236f8f58ea6408ec46eb55
Gabriel Batista dos Santos <86369237+Gabri3lBatista@users.noreply.github.com>
2025-04-01 10:41:05 -0500

    eu sei que vamos conseguir

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from products.models import Product
+from django.forms.models import model_to_dict 
 
-# Create your views here.
+
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    
+        
+    return JsonResponse({"message": "Hi, there!"})
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -1,3 +1,6 @@
 import requests
 
-endpoint = ""
+endpoint = "http://127.0.0.1:8000/api/"
+get_reponse = requests.get(endpoint)
+
+print(get_reponse.json())
